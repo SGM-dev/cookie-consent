b450fd5af62a6c5889dd1955f98eba109b7b9b32
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-06-19 15:38:17 +0530

    add modal message using createElement() and co.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const modal = document.getElementById("modal");
 const closeModalBtn = document.querySelector("#modal-close-btn");
 const consentFormEl = document.getElementById("consent-form");
+const modalTextEl = document.getElementById("modal-text");
+
+const innerLoadingModal = document.createElement("div");
+innerLoadingModal.classList.add("modal-inner-loading");
+
+const loaderImgEl = document.createElement("img");
+loaderImgEl.setAttribute("src", "images/loading.svg");
+loaderImgEl.classList.add("loading");
+
+const uploadTextEl = document.createElement("p");
+uploadTextEl.setAttribute("id", "uploadText");
+uploadTextEl.textContent = "Uploading your data to the dark web...";
+
+innerLoadingModal.prepend(loaderImgEl);
+innerLoadingModal.append(uploadTextEl);
 
 setTimeout(function () {
   modal.style.display = "inline";
@@ -13,4 +28,6 @@ closeModalBtn.addEventListener("click", () => {
 consentFormEl.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("form submitted");
+  modalTextEl.replaceChildren(); // clear children of modalTextEl
+  modalTextEl.append(innerLoadingModal);
 });
