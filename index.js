bf694c4ab3d091bcfde77374d7bf4f98594679c0
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-06-19 15:13:32 +0530

    prevent default behavior of form submission

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const modal = document.getElementById("modal");
+const closeModalBtn = document.querySelector("#modal-close-btn");
+const consentFormEl = document.getElementById("consent-form");
 
 setTimeout(function () {
   modal.style.display = "inline";
 }, 1500);
 
-const closeModalBtn = document.querySelector("#modal-close-btn");
-
 closeModalBtn.addEventListener("click", () => {
   modal.style.display = "none";
+});
+
+consentFormEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("form submitted");
 });
