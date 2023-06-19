d2d1558100de1e51111432dfbd85d1f07da250d1
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-06-19 11:52:39 +0530

    close the modal on clicking close button

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const modal = document.getElementById("modal");
 setTimeout(function () {
   modal.style.display = "inline";
 }, 1500);
+
+const closeModalBtn = document.querySelector("#modal-close-btn");
+
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
