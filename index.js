ca620111951b085ed3397dcd1a9b23a585df23bd
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-06-19 16:15:47 +0530

    store FormData in a const variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const modal = document.getElementById("modal");
 const closeModalBtn = document.querySelector("#modal-close-btn");
-const consentFormEl = document.getElementById("consent-form");
-const modalTextEl = document.getElementById("modal-text");
+const consentForm = document.getElementById("consent-form");
+const modalText = document.getElementById("modal-text");
 
 setTimeout(function () {
   modal.style.display = "inline";
@@ -11,9 +11,12 @@ closeModalBtn.addEventListener("click", () => {
   modal.style.display = "none";
 });
 
-consentFormEl.addEventListener("submit", (e) => {
+consentForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  modalTextEl.innerHTML = `<div class="modal-inner-loading"><img src="images/loading.svg" class="loading"><p id="uploadText">Uploading your data to the dark web...</p></div>`;
+
+  const consentFormData = new FormData(consentForm);
+
+  modalText.innerHTML = `<div class="modal-inner-loading"><img src="images/loading.svg" class="loading"><p id="uploadText">Uploading your data to the dark web...</p></div>`;
 
   setTimeout(() => {
     document.getElementById("uploadText").textContent = `Making the sale...`;
